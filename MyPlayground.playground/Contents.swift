313d420aa7a9bba7cfb96d4f0fbc4289914e82f2
Jaqueline <jaquelinesantos2810@gmail.com>
2021-03-12 01:34:45 -0300

    Aprendendendo chamada e declarações de  funções e sintaxe da linguagem

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,23 @@
 import UIKit
 
-var nome = "Jaqueline Bittencourt"
+var nome = "maçã"
+let felicidade = 5
+let calorias: Double = 79.5
+
+let vegetal: Bool = false
+
 
 var endereco = "Uberlandia"
 
-print(nome)
+//
+//func alimentoConsumido(){
+//    print("o alimento consumido foi: \(nome)")
+//}
+//alimentoConsumido()
+//alimentoConsumido()
+
+func alimentoConsumido(_ nome: String, _ calorias: Double, _ felicidade: Int){
+    print("o alimento consumido foi \(nome), com: \(calorias) calorias e a felicidade foi nível \(felicidade)")
+}
+
+ alimentoConsumido(nome, calorias, felicidade)
