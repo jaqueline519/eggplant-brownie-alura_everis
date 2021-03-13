21c5bd3a2d25e5e11d931e6cc994249554b21ceb
Jaqueline <jaquelinesantos2810@gmail.com>
2021-03-12 21:45:30 -0300

    Alterações no playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,42 +1,48 @@
+
 import UIKit
 
 class Refeicao {
-    var nomeAlimento: String?
-    var felicidade: String?
-}
-//instanciar a classe
-
-let refeicao = Refeicao()
-refeicao.nomeAlimento = "Banana"
-
-//cuidado com o forced unwrap
-
-/*forma de verificar se o valor opcional é nulo ( esta forma não costuma ser utilizada"
- */
-
-if refeicao.nomeAlimento != nil {
- 
- print(refeicao.nomeAlimento!)
-}
+    var nomeAlimento: String
+    var qtdfelicidade: String
+    var itens: Array<Item> = []
 
 
-//Boas práticas para extrair valores opcionais:
-
-if let nome = refeicao.nomeAlimento {
-     print(nome)
-}
-
-//guard let (maneira mais utilizada)
-
-func exibNomeDaRefeicao() {
-    if let nome = refeicao.nomeAlimento{
-        print(nome)
+    init(nome: String, felicidade: String) {
+        self.nomeAlimento = nome
+        self.qtdfelicidade = felicidade
     }
-    guard  let nome1 = refeicao.nomeAlimento else {
-        return
+    func totalDeCalorias() -> Double {
+        var total = 0.0
+
+        for item in itens {
+            total += item.calorias
+        }
+
+        return total
     }
-    print(nome1)
 }
 
-exibNomeDaRefeicao()
+class Item {
+    var nome: String
+    var calorias: Double
+
+    init(nome: String, calorias: Double) {
+        self.nome = nome
+        self.calorias = calorias
+    }
+}
+let arroz = Item(nome: "Arroz", calorias: 51.0)
+let feijao = Item(nome: "Feijão", calorias: 90.0)
+let contraFile = Item(nome: "Contra Filé", calorias: 287.0)
+
+let refeicao = Refeicao(nome: "Almoço", felicidade: "5")
+refeicao.itens.append(arroz)
+refeicao.itens.append(feijao)
+refeicao.itens.append(contraFile)
+
+print(refeicao.nomeAlimento)
+if let primeiroItemDaLista = refeicao.itens.first {
+    print(primeiroItemDaLista.nome)
+}
+print(refeicao.totalDeCalorias())
 
